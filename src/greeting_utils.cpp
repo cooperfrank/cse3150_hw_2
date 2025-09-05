a4b900c3f601b573075d8c6dacc1b65473c4e3ad
Cooper Frank <coopfrank04@gmail.com>
2025-09-04 21:23:38 -0400

    Created greeting_utils.cpp file with function implementations.

---
+++ src/greeting_utils.cpp
@@ -0,0 +1,24 @@
+#include <string>
+#include "greeting_utils.h"
+
+namespace GreetingUtils {
+    std::string create_message(const std::string& name) {
+        std::string msg = "Hello, " + name;
+        return msg;
+    }
+
+    char* format_as_c_string(const std::string& msg) {
+        std::size_t msg_len = msg.length();
+        std::size_t buf_len = msg_len + 1;
+
+        char* buf = new char[buf_len];
+
+        for(size_t i = 0; i < msg_len; i++) {
+            buf[i] = msg[i];
+        }
+
+        buf[buf_len - 1] = '\0';
+
+        return buf;
+    }
+}
